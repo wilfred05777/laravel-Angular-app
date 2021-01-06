b1b4f519335186907bae2dae41b8c4880ebc4ef9
Wilfred Erdo <wilfred05777@users.noreply.github.com>
2021-01-06 09:03:20 +0800

    AngularController and route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::any('/{any}', [AngularController::class, 'index'])->where('any', '^(?!api).*$');
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
